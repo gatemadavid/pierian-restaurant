0d7855169e3d8bfa423e968844807a4072c09dea
Gatema David <dshefard@gmail.com>
2018-08-24 21:11:21 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+//responsive nav
+$(function() {
+  menu = $("nav ul");
+
+  $("#openup").on("click", function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function() {
+    var w = $(this).width();
+    if (w > 480 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+  });
+
+  $("nav li").on("click", function(e) {
+    var w = $(window).width();
+    if (w < 480) {
+      menu.slideToggle();
+    }
+  });
+  $(".open-menu").height($(window).height());
+});
+
+// Smooth Scrolling
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+var modal = document.getElementById("simpleModal");
+var modalBtn = document.getElementById("modalBtn");
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+modalBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+//listen for outside click
+window.addEventListener("click", clickOutside);
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+    submodal.style.display = "none";
+  }
+}
+
+var submodal = document.getElementById("submodal");
+var subBtn = document.getElementById("subscribe-btn");
+var subClose = document.getElementsByClassName("sub-close");
+
+subBtn.addEventListener("clicl", openSubModal);
+subClose.addEventListener("click", closeSubModal);
+
+function openSubModal() {
+  submodal.style.display = "block";
+}
+
+function closeSubModal() {
+  submodal.style.display = "none";
+}
